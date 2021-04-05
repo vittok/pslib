9df7b9c99c02a27c5e00f09ee9b48070e6776e8c
vittok <46328471+vittok@users.noreply.github.com>
2021-04-05 11:42:05 +0200

    Update updatewindows.ps1

--- updatewindows.ps1
+++ updatewindows.ps1
@@ -1,6 +1,7 @@
 # This script installs update module and sets it to pull updates for Microsoft products (not just Windows)
 Set-ExecutionPolicy RemoteSigned
 # Install modules
+Install-Module PSWindowsUpdate
 Import-Module PSWindowsUpdate
 Add-WUServiceManager -MicrosoftUpdate
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll
